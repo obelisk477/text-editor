9e24363ecfcdfe2d431a78f1d435dfeccfbb7163
obelisk477 <cwbatte2@gmail.com>
2023-08-01 00:51:30 -0400

    More bugfixing in boilerplate; app now works on and offline

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,20 +17,20 @@ export const putDb = async (content) => {
   console.log('Put / update to the database');
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('contact', 1);
+  const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readwrite');
+  const tx = jateDb.transaction('jate', 'readwrite');
 
   // Open up the desired object store.
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email });
+  const request = store.put({ id: 1, stringifiedCode: content});
 
   // Get confirmation of the request.
   const result = await request;
-  console.log('Data saved to the database', result);
+  console.log('Data updated in the database', result);
 }
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -52,7 +52,7 @@ export const getDb = async () => {
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result.length? result : '';
+  return result.length? result[0].stringifiedCode : '';
 };  
 
 
--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -22,6 +22,8 @@ export default class {
       tabSize: 2,
     });
 
+    
+
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
