a985e6b9edda63757dcec9f3a8bac05ba5b21ffe
obelisk477 <cwbatte2@gmail.com>
2023-08-01 01:23:50 -0400

    Added css to hide install button after install

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,7 +3,6 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    
     // Store the triggered events
     window.deferredPrompt = event;
 
@@ -12,7 +11,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {      
+butInstall.addEventListener('click', async () => {
   const promptEvent = window.deferredPrompt;
 
   if (!promptEvent) {
@@ -20,7 +19,7 @@ butInstall.addEventListener('click', async () => {
   }
 
   // Show prompt
-  promptEvent.prompt();
+  await promptEvent.prompt();
   
   // Reset the deferred prompt variable, it can only be used once.
   window.deferredPrompt = null;
