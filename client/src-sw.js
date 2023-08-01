a71a9b687e3868338d225216d9fb56429199933d
obelisk477 <cwbatte2@gmail.com>
2023-08-01 12:26:51 -0400

    Added image caching to fix icon not loading offline

--- client/src-sw.js
+++ client/src-sw.js
@@ -38,3 +38,19 @@ registerRoute(
     ],
   })
 );
+
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'my-image-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
